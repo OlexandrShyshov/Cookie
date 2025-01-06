c0907db16e3a1d8867644f6ada58e31213a444f4
s.shyshov <155887046+OlexandrShyshov@users.noreply.github.com>
2025-01-06 13:40:30 +0200

    add commit
    cleaned up the code in some files

--- src/js/index.js
+++ src/js/index.js
@@ -2,26 +2,10 @@
 import { addLoadedClass, addTouchClass, menuInit } from "./moduls/functions.js";
 import { headerScroll, digitsCounter, pageNavigation } from "./files/scroll/scroll.js";
 
-/* importing libs */
-//import './libs/wNumb.js';
+/* Importing libs */
 import './libs/watcher.js';
-import './libs/dynamic_adapt.js';
 
-
-/* ==== Adding the touch class to HTML if the browser is mobile ==== */
-//addTouchClass()
-
-/* ==== Adding the loaded class to HTML after the page is fully loaded ==== */
+/* functions */
 addLoadedClass()
-
-/* ==== Adding a menu (burger) to a page ==== */
 menuInit()
-
-/* ==== Adding classes to the header while scrolling ==== */
 headerScroll()
-
-/* ==== Digital counter animations ==== */
-//digitsCounter()
-
-/* ==== Smooth page navigation ==== */
-//pageNavigation()
